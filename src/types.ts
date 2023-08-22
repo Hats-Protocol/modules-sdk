5e19e582a8c154d5418d0f3e990ffd21396d0559
Ido <gershido@gmail.com>
2023-08-22 10:51:17 +0300

    remove unused type

--- src/types.ts
+++ src/types.ts
@@ -9,8 +9,6 @@ export interface CreateInstanceResult extends TransactionResult {
   newInstance: `0x${string}`;
 }
 
-export type SupportedChain = "1" | "5";
-
 export type CreateInstanceArg = {
   name: string;
   description: string;
