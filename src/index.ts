0eed13072300795ffb89290d59c0c59cb8dd1b2b
Ido <gershido@gmail.com>
2023-08-23 22:48:15 +0300

    fix export bug

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { HatsModulesClient } from "./client";
 import { verify, getSchema, solidityToTypescriptType } from "./schemas";
-import type { Module, Factory } from "types";
+import type { Module, Factory } from "./types";
 
 export { HatsModulesClient, verify, getSchema, solidityToTypescriptType };
 export type { Module, Factory };
